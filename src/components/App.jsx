afd08223ecc92337b8ba66fffe2c201bb196c06c
TanishqSingla <tanishqsingla00@gmail.com>
2020-04-10 00:47:59 +0530

    making notes dynamic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note title="Title" content="This is the content" />
+      {notes.map((note) => (
+        <Note key={note.id} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
---
+++ src/notes.js
@@ -0,0 +1,9 @@
+const notes = [
+  {
+    id: 1,
+    title: "Title",
+    content: "Content",
+  },
+];
+
+export default notes;
