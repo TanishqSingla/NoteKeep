31cb05350f14e6e792a184f80d5c11d983c0ffac
TanishqSingla <tanishqsingla00@gmail.com>
2020-04-27 01:32:37 +0530

    Adding event handler for input fields

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateNote = () => {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  };
+
   return (
     <div>
       <form>
-        <input type="text" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." />
+        <input
+          name="title"
+          type="text"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          value={note.content}
+          placeholder="Take a note..."
+        />
         <button>+</button>
       </form>
     </div>
